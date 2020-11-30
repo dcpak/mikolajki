ea85c9e6cb9844ea943d46e8eef7439bc06839db
Przemysław Kasprzak <przemyslaw.kasprzak@gmail.com>
2020-11-30 16:42:36 +0100

    Fixed cross matching.

--- index.php
+++ index.php
@@ -23,11 +23,16 @@
 
     body > div {
       text-align: center;
+      padding-bottom: 20px;
     }
 
     body > div span {
       font-family: Arial, serif;
       font-size: 5em;
+    }
+
+    body > div.error {
+      color: #f00;
     }
 
     input {
@@ -85,68 +90,85 @@
 <?php
 const NUMBER_OF_PEOPLE = 3;
 const FILE_NUMBERS = 'numbers_taken.txt';
-const ALERT_ERROR_WRITE = '<div>Nie udało się zapisać wylosowanego numerka, rocketchatuj do administratora.</div>';
-const ALERT_ERROR_NUMBER = '<div>Numer spoza zakresu.</div>';
-const ALERT_ERROR_PASSWORD = '<div>Numer dobry, tylko hasło nie bardzo.<div>';
+const ALERT_ERROR_WRITE = '<div class="error">Nie udało się zapisać wylosowanego numerka.<br />Spróbuj jeszcze raz, albo rocketczatuj do administratora.</div>';
+const ALERT_ERROR_NO_PARAMS = '<div class="error">O jerum pajtasz, czegoś tu zabrakowało.</div>';
+const ALERT_ERROR_NUMBER_FLOAT = '<div class="error">Nijaki taki ten numer.</div>';
+const ALERT_ERROR_NUMBER_RANGE = '<div class="error">Numer taki trochę nieszczególny.</div>';
+const ALERT_ERROR_PASSWORD = '<div class="error">Numer dobry, tylko hasło niewydarzone.</div>';
 const TPL_INFO_NUMBER = '<div>Twój szczęśliwy numerek:<br /><span><number/></span></div>';
 
+$showForm = true;
 if(@$_POST['number'] != '' && @$_POST['pswd'] != '') {
   $numberUser = intval($_POST['number']);
-  if($numberUser > 0 && $numberUser <= NUMBER_OF_PEOPLE) {
-    $numberExists = false;
-    $numberShow = false;
-    $numbersTaken = array();
-    $numbersMatches = array();
-    if(file_exists(FILE_NUMBERS)) {
-      $lines = file(FILE_NUMBERS, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
-      foreach($lines as $line) {
-        $line = explode(' ', $line);
-        $numbersTaken[] = $line[2];
-        $numbersMatches[$line[2]] = $line[0];
-        if(password_verify($_POST['number'], $line[0])) {
-          $numberExists = true;
-          if(password_verify($_POST['pswd'], $line[1]))
-            echo str_replace('<number/>', $line[2], TPL_INFO_NUMBER);
-          else
-            echo ALERT_ERROR_PASSWORD;
+  if($numberUser == $_POST['number']) {
+    if($numberUser > 0 && $numberUser <= NUMBER_OF_PEOPLE) {
+      $numberExists = false;
+      $numberShow = false;
+      $numbersTaken = array();
+      $numbersMatches = array();
+      if(file_exists(FILE_NUMBERS)) {
+        $lines = file(FILE_NUMBERS, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+        foreach($lines as $line) {
+          $line = explode(' ', $line);
+          $numbersTaken[] = $line[2];
+          $numbersMatches[$line[2]] = $line[0];
+          if(password_verify($numberUser, $line[0])) {
+            $numberExists = true;
+            if(password_verify($_POST['pswd'], $line[1])) {
+              echo str_replace('<number/>', $line[2], TPL_INFO_NUMBER);
+              $showForm = false;
+            } else {
+              echo ALERT_ERROR_PASSWORD;
+            }
+          }
         }
       }
-    }
-    if(!$numberExists) {
-      $numbersAvailable = array();
-      for($i = 1; $i <= NUMBER_OF_PEOPLE; $i++) {
-        if(!in_array($i, $numbersTaken)
-          && $i != $numberUser
-          && !(
-            isset($numbersMatches[$numberUser])
-            && password_verify($i, $numbersMatches[$numberUser])
-          )
-        )
-          $numbersAvailable[] = $i;
-      }
-      $numberTaken = $numbersAvailable[rand(0, count($numbersAvailable) - 1)];
-      $hashes = array();
-      $hashes[] = password_hash($_POST['number'], PASSWORD_DEFAULT);
-      $hashes[] = password_hash($_POST['pswd'], PASSWORD_DEFAULT);
-      if($hashes[0] && $hashes[1]) {
-        $fp = @fopen(FILE_NUMBERS, 'a');
-        if($fp === false) {
-          echo ALERT_ERROR_WRITE;
-        } else {
-          if(@fwrite($fp, $hashes[0].' '.$hashes[1].' '.$numberTaken."\n") === false)
+      if(!$numberExists) {
+        $numbersAvailable = array();
+        for($i = 1; $i <= NUMBER_OF_PEOPLE; $i++)
+          if(!in_array($i, $numbersTaken) && $i != $numberUser) {
+            $allowed = true;
+            foreach($numbersMatches as $key => $value)
+              if(password_verify($i, $value))
+                $allowed = false;
+            if($allowed)
+              $numbersAvailable[] = $i;
+          }
+        if(!count($numbersAvailable))
+          for($i = 1; $i <= NUMBER_OF_PEOPLE; $i++)
+            if(!in_array($i, $numbersTaken) && $i != $numberUser)
+              $numbersAvailable[] = $i;
+        $numberTaken = $numbersAvailable[rand(0, count($numbersAvailable) - 1)];
+        $hashes = array();
+        $hashes[] = password_hash($numberUser, PASSWORD_DEFAULT);
+        $hashes[] = password_hash($_POST['pswd'], PASSWORD_DEFAULT);
+        if($hashes[0] && $hashes[1]) {
+          $fp = @fopen(FILE_NUMBERS, 'a');
+          if($fp === false) {
             echo ALERT_ERROR_WRITE;
-          else
-            echo str_replace('<number/>', $numberTaken, TPL_INFO_NUMBER);
-          @fclose($fp);
+          } else {
+            if(@fwrite($fp, $hashes[0].' '.$hashes[1].' '.$numberTaken."\n") === false) {
+              echo ALERT_ERROR_WRITE;
+            } else {
+              echo str_replace('<number/>', $numberTaken, TPL_INFO_NUMBER);
+              $showForm = false;
+            }
+            @fclose($fp);
+          }
+        } else {
+          echo ALERT_ERROR_WRITE;
         }
-      } else {
-        echo ALERT_ERROR_WRITE;
       }
+    } else {
+      echo ALERT_ERROR_NUMBER_FLOAT;
     }
   } else {
-    echo ALERT_ERROR_NUMBER;
+    echo ALERT_ERROR_NUMBER_RANGE;
   }
 } else {
+  echo ALERT_ERROR_NO_PARAMS;
+}
+if($showForm) {
   ?>
 <form name="formSelect" method="post" action="index.php">
   <section>
